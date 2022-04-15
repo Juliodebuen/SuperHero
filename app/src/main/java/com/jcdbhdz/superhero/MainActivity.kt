b8a76548553aa56a874f7323ff950a80ba86e2c2
juliodebuen <julio_halo10@hotmail.com>
2022-04-15 10:22:49 -0500

    initial commit - base project

---
+++ app/src/main/java/com/jcdbhdz/superhero/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.jcdbhdz.superhero
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.jcdbhdz.superhero.ui.theme.SuperHeroTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SuperHeroTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Composable
+fun DefaultPreview() {
+    SuperHeroTheme {
+        Greeting("Android")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SuperHeroList(){
+    LazyColumn{
+      /*  items(){
+
+        }*/
+    }
+}
