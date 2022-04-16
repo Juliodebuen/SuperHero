190a4d42da0f828c81977457b79081e15240d81d
juliodebuen <julio_halo10@hotmail.com>
2022-04-16 15:31:40 -0500

    new services were added

--- app/src/main/java/com/jcdbhdz/superhero/data/remote/SuperHeroApiRest.kt
+++ app/src/main/java/com/jcdbhdz/superhero/data/remote/SuperHeroApiRest.kt
@@ -1,7 +1,9 @@
 package com.jcdbhdz.superhero.data.remote
 
-import com.jcdbhdz.superhero.data.model.CharacterDataWrapper
+import com.jcdbhdz.superhero.data.model.*
+import kotlinx.coroutines.flow.MutableStateFlow
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface SuperHeroApiRest {
@@ -14,4 +16,44 @@ interface SuperHeroApiRest {
         @Query("hash") hash:String,
         @Query("ts") ts:String
     ) : CharacterDataWrapper
+
+    @GET("/v1/public/characters/{charactedId}")
+    suspend fun getCharacterById(
+        @Path("charactedId") characterId: String,
+        @Query("apikey")apiKey:String,
+        @Query("hash") hash:String,
+        @Query("ts") ts:String
+    ) : CharacterDataWrapper
+
+    @GET("/v1/public/characters/{charactedId}/comics")
+    suspend fun getComicsByCharacterId(
+        @Path("charactedId") characterId: String,
+        @Query("apikey")apiKey:String,
+        @Query("hash") hash:String,
+        @Query("ts") ts:String
+    ) : ComicDataWrapper
+
+    @GET("/v1/public/characters/{charactedId}/events")
+    suspend fun getEventsByCharacterId(
+        @Path("charactedId") characterId: String,
+        @Query("apikey")apiKey:String,
+        @Query("hash") hash:String,
+        @Query("ts") ts:String
+    ) : EventDataWrapper
+
+    @GET("/v1/public/characters/{charactedId}/series")
+    suspend fun getSeriesByCharacterId(
+        @Path("charactedId") characterId: String,
+        @Query("apikey")apiKey:String,
+        @Query("hash") hash:String,
+        @Query("ts") ts:String
+    ) : SeriesDataWrapper
+
+    @GET("/v1/public/characters/{charactedId}/stories")
+    suspend fun getStoriesByCharacterId(
+        @Path("charactedId") characterId: String,
+        @Query("apikey")apiKey:String,
+        @Query("hash") hash:String,
+        @Query("ts") ts:String
+    ) : StoryDataWrapper
 }
